bcbf202d6091ef716d0715df1d5e2b3ee31cefd5
unknown <satya0450@gmail.com>
2019-05-01 23:12:12 +0530

    add modal in the header.js file on login click to show modal and close function added

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,19 +1,38 @@
-import React ,{Component}from 'react';
+import React, { Component } from 'react';
 
 import './Header.css';
 import Button from '@material-ui/core/Button';
 import logo from '../../assets/logo.svg';
+import Modal from 'react-modal';
 
 class Header extends Component {
 
+    constructor(){
+        super();
+        this.state={
+            modalIsOpen:false
+        };
+    }
+    openModalHandler=()=>{
+           this.setState({modalIsOpen:true})
+    }
+    closeModalHandler=()=>{
+        this.setState({modalIsOpen:false})
+    }
     render() {
         return (
-            <div className="app-header">
-
-            <img src={logo} className="app-logo" alt="app-log"/>
-                <Button variant="contained" color="default" className="login-button">
-                    Login
+            <div>
+                <header className="app-header">
+                    <img src={logo} className="app-logo" alt="app-log" />
+                    <div className="login-button">
+                    <Button variant="contained" color="default" onClick={this.openModalHandler} className="login-button">
+                        Login
                 </Button>
+                </div>
+                </header>
+                <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login" onRequestClose={this.closeModalHandler}>
+
+                </Modal>
             </div>
         )
     }
