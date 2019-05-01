e57b93c0a9653dbff5f7e2ef99bf4e6722b17d0b
unknown <satya0450@gmail.com>
2019-05-01 22:34:53 +0530

    material button is added in new folder under common -->Header -->Header.css & Header.js fiels

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -2,13 +2,16 @@ import React ,{Component}from 'react';
 
 import './Header.css';
 import Button from '@material-ui/core/Button';
+import logo from '../../assets/logo.svg';
 
 class Header extends Component {
 
     render() {
         return (
-            <div>
-                <Button variant="contained" color="default">
+            <div className="app-header">
+
+            <img src={logo} className="app-logo" alt="app-log"/>
+                <Button variant="contained" color="default" className="login-button">
                     Login
                 </Button>
             </div>
