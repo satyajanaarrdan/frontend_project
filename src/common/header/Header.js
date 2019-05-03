21358cd2b596717246e4bbcc714f841d4abf43d5
unknown <satya0450@gmail.com>
2019-05-03 08:27:14 +0530

    added popup/Modal on click of login to display login and register tabs in Header.js file

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -4,20 +4,40 @@ import './Header.css';
 import Button from '@material-ui/core/Button';
 import logo from '../../assets/logo.svg';
 import Modal from 'react-modal';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+
+const customStylesForModal={
+    content:{
+        top:"50%",
+        left:"50%",
+        right:"auto",
+        bottom:"auto",
+        marginRight:'-100%',
+        transform:'translate(50%,-50%)'
+    }
+}
 
 class Header extends Component {
 
     constructor(){
         super();
         this.state={
-            modalIsOpen:false
+            modalIsOpen:false,
+            value:0
         };
     }
+
+
     openModalHandler=()=>{
            this.setState({modalIsOpen:true})
     }
     closeModalHandler=()=>{
         this.setState({modalIsOpen:false})
+    }
+    tabChangeHandler=(event,value)=>{
+            this.setState({value})
     }
     render() {
         return (
@@ -30,8 +50,13 @@ class Header extends Component {
                 </Button>
                 </div>
                 </header>
-                <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login" onRequestClose={this.closeModalHandler}>
-
+                <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen}
+                     contentLabel="Login" onRequestClose={this.closeModalHandler}
+                        style={customStylesForModal}>
+                    <Tabs value={this.state.value} onChange={this.tabChangeHandler} >
+                        <Tab label="Login"/>
+                        <Tab label="Register"/>
+                    </Tabs>
                 </Modal>
             </div>
         )
