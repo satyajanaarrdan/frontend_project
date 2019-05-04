2767a8dbdb2819fa0c2ace4d6fffbfefdf157e86
unknown <satya0450@gmail.com>
2019-05-04 21:53:15 +0530

    add login tab input controls in the formcontrol for each ctrl, under Typography which used to indicate formctrl create a function which passes values to the props to uses prop.children in the return() to render formctrl tabs in the tab

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './screens/Home/Home';
-
+import 'typeface-roboto';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
