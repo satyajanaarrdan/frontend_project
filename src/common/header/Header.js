c578ed37814f141861b5a83104aad955e96ed280
unknown <satya0450@gmail.com>
2019-05-05 16:21:54 +0530

    add all files

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -45,38 +45,55 @@ class Header extends Component {
             modalIsOpen:false,
             value:0,
             userNameRequired:"dispNone",
-            username:""
+            passwordRequired:"dispNone",
+            username:"",
+            password:"",
+            isOpen:true
         };
     }
 
 
     openModalHandler=()=>{
-           this.setState({modalIsOpen:true})
+           this.setState({modalIsOpen:true});
+
+           this.setState({
+            modalIsOpen: true,
+            value: 0,
+            usernameRequired: "dispNone",
+            passwordRequired:"dispNone",
+            password:"",
+            username: ""
+        });
     }
     closeModalHandler=()=>{
-        this.setState({modalIsOpen:false})
-    }
+        this.setState({modalIsOpen:false});
+        }
     tabChangeHandler=(event,value)=>{
-            this.setState({value})
+            this.setState({value});
     }
 
     loginClickHandler=()=>{
-        this.state.username === "" ? this.setState({userNameRequired:"dispBlock"}) :this.setState({userNameRequired:"dispNone"});
+        this.state.username === "" ? this.setState({userNameRequired:"dispBlock"}) :this.setState({userNameRequired:"dispNone"});    
+        this.state.password === "" ? this.setState({passwordRequired:"dispBlock"}) :this.setState({passwordRequired:"dispNone"});  
     }
     inputUserNameChangeHandler=(e)=>{
             this.setState({userName:e.target.value})
-    }
+        }
+        inputPasswordChangeHandler=(e)=>{
+            this.setState({password:e.target.value});
+        }
 
+       
     render() {
         return (
             <div>
                 <header className="app-header">
                     <img src={logo} className="app-logo" alt="app-log" />
                     <div className="login-button">
-                    <Button variant="contained" color="default" onClick={this.openModalHandler} className="login-button">
+                      <Button variant="contained" color="default"  onClick={this.openModalHandler} className="login-button">
                         Login
-                </Button>
-                </div>
+                     </Button>
+                    </div>
                 </header>
                 <Modal ariaHideApp={false} isOpen={this.state.modalIsOpen}
                      contentLabel="Login" onRequestClose={this.closeModalHandler}
@@ -91,7 +108,7 @@ class Header extends Component {
                         will render to the browser */}
                         <FormControl required>
                             <InputLabel htmlFor="username">UserName </InputLabel>
-                            <Input id="username" type="text" userName={this.state.username} onChange={this.inputUserNameChangeHandler}/>
+                            <Input id="username" type="text" username={this.state.username} onChange={this.inputUserNameChangeHandler}/>
                             
                                 <FormHelperText className={this.state.userNameRequired}>
                                     <span className="red">Required</span>
@@ -100,11 +117,15 @@ class Header extends Component {
                             <br/><br/>
                         <FormControl>
                             <InputLabel htmlFor="password">Password </InputLabel>
-                            <Input id="password" type="password"/>
+                            <Input id="password" type="password" password={this.state.password} onChange={this.inputPasswordChangeHandler}/>
+                            <FormHelperText className={this.state.passwordRequired} >
+                                    <span className="red">Required</span>
+                                 </FormHelperText>
                         </FormControl>
                         <br/>
                         <br/>
                         <Button variant="contained" color="primary" onClick={this.loginClickHandler}>LOGIN</Button>
+
                     </TabContainer>
                     }
                 </Modal>
